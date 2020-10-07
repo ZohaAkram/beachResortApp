0c1b1ada9e56ae2e6bd065f42e8f59bcd1be51e0
ZohaAkram <zoha.ak24@gmail.com>
2020-10-07 14:56:56 +0500

    loading files

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from './Loading';
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    
+  
 
     return (
       <div>
         from featured rooms
+        <Loading/>
       </div>
+
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -9,7 +9,22 @@ export default class RoomProvider extends Component {
 featuredRooms:[],
 loading:true
   };
-  
+  componentDidMount(){
+  let rooms= this.formatData(items);
+  let featuredRooms=rooms.filter(room=>room.featured===true);
+  this.setState({
+    rooms,featuredRooms,sortedRooms:rooms,loading:false
+  });
+  }
+  formatData(items){
+    let tempItems=items.map(item=>{
+      let id=item.sys.id;
+      let images=item.fields.images.map(image=>image.fields.file.url);
+      let room={...item.fields,images,id}
+      return room;
+    });
+    return tempItems
+  }
   render() {
     return (
       <RoomContext.Provider value={{...this.state}}>
